f148f3a3ec45c89b8ce7b87d05adaadc0edb94a7
Klefebvre <klefebvre.dev@gmail.com>
2021-05-05 16:13:54 +0200

    site V.1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-const app = document.querySelector(".movie-list");
+const app = document.querySelector("#movie-list");
+const movieList = document.getElementById("movie-list");
 let movies = [];
 let input = document.getElementById("searchresult");
 let search;
+let form = document.getElementById("form");
+console.log(form);
 
 const fetchMovie = async () => {
   await fetch(
@@ -10,26 +13,50 @@ const fetchMovie = async () => {
   )
     .then((res) => res.json())
     .then((data) => (movies = data.results));
-
-  console.log(movies);
-  input.addEventListener("input", (event) => {
-    search = event.target.value;
-    fetchMovie();
-  });
 };
-const moviesDisplay = () => {};
 
-// app.innerHTML = fetchMovie
-//   .map((movie) => {
-//     return `
-// 		<li class="movie">
-// 		<h2>${title}</h2>
-// 		<img src= alt='photo de/>
-// 		<p></p>
-// 		<p></p>
-// 		</li>
-// 		`;
-//   })
-//   .join("");
+input.addEventListener("input", (event) => {
+  search = event.target.value;
+  fetchMovie();
+});
 
-// fetchMovie();
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  moviesDisplay();
+});
+
+const dateParser = (date) => {
+  let newDate = new Date(date).toLocaleDateString("fr-FR", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+
+  console.log(dateParser);
+  return newDate;
+};
+
+const moviesDisplay = () => {
+  app.innerHTML = movies
+    .map((movie) => {
+      return `
+    <li class="movie">
+    <h2>${movie.title}</h2>
+    <p>Date de sortie : ${dateParser(movie.release_date)}</p>
+    <img class="affiche" src=${
+      movie.poster_path
+        ? "https://image.tmdb.org/t/p/w300/" + movie.poster_path
+        : "assets/img/poster.jpg"
+    }
+    alt="Affiche du film ${movie.title} "/>
+    <p>Note du film : ${movie.vote_average}/10 <img  class=logofg src=${
+        movie.vote_average < 6 ? "assets/img/glacon.png" : "assets/img/fire.png"
+      }></p>
+    <p class="syn">Synopsis : ${movie.overview}</p>
+    </li>
+    `;
+    })
+    .join("");
+};
+
+// ${movie.poster_path} != null ? "https://image.tmdb.org/t/p/w300${movie.poster_path}" : "..assets/img/poster.jpg"}
